5688947be641109192cd958c47d4cc011a5e63cd
Everson Santos <everson94santos@gmail.com>
2023-02-25 13:49:18 -0300

    first commit

---
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DesafioFundamentos.Models
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial;
+        private decimal precoHora;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoHora = precoHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para estacionar:");            
+            var placa = Console.ReadLine().ToUpper();
+            bool validaPlaca = Regex.Match(placa, @"(\D{3})\-?\s?(\d{4}|\d{1}\D{1}\d{2})").Success;
+            if(!validaPlaca || placa.Length > 8)
+                Console.WriteLine("A placa informada não é valida!");
+            else
+            {
+                veiculos.Add(placa);
+                Console.WriteLine("Veículo adicionado com sucesso!");
+            }
+            
+        }
+
+        public void ListarVeiculos()
+        {
+            if(veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são:");
+                foreach (var veiculo in veiculos)
+                {
+                    Console.WriteLine(veiculo);
+                }
+            }
+            else
+                Console.WriteLine("Não há veículos estacionados!");
+
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veiculo para remover:");
+            var placa = Console.ReadLine().ToUpper();
+
+            if(veiculos.Any(v => v.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                if(!Int32.TryParse(Console.ReadLine(),out int quantidadeHoras))
+                    Console.WriteLine("Valor inválido!");
+                else
+                {
+                    decimal precoTotal = (quantidadeHoras * precoHora) + precoInicial;
+                    veiculos.Remove(placa);
+
+                    Console.WriteLine($"O veiculo {placa} foi removido e o preço total foi de R$ {precoTotal}.");
+                }
+
+            }
+            else
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+
+        }
+    }
+}
---
+++ DesafioFundamentos/Program.cs
@@ -0,0 +1,51 @@
+﻿using DesafioFundamentos.Models;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal precoInicial = 0;
+decimal precoHora = 0;
+
+Console.WriteLine("Seja bem vindo sistema de estacionamento! \n" +
+    "Digite o preço inicial: ");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora:");
+precoHora = Convert.ToDecimal(Console.ReadLine());
+
+var estacionamento = new Estacionamento(precoInicial, precoHora);
+var opcao = string.Empty;
+bool exibirMenu = true;
+
+while(exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("=========================================");
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Lista veículos");
+    Console.WriteLine("4 - Encerrar");
+    Console.WriteLine("=========================================");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            estacionamento.AdicionarVeiculo();
+            break;
+        case "2":
+            estacionamento.RemoverVeiculo();
+            break;
+        case "3":
+            estacionamento.ListaVeiculos();
+            break;
+        case "4":
+            exibirMenu= false;
+            break;
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    }
+    Console.WriteLine("Pressione um tecla para continuar.");
+    Console.ReadLine();
+}
+Console.WriteLine("O programa se encerrou!");
